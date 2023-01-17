efc47f2e3eb7dd868f5c5b0f63ed89218a8965c7
DChelin <devlynchelin6@gmail.com>
2023-01-17 14:29:09 +0200

    PHP REST API Notes
    17-Jan-2023

---
+++ notes.php
@@ -0,0 +1,24 @@
+<?php
+
+//REST API
+// Representational State Transfer
+// Relies on stateless , client-server protocol, almost always HTTP
+// Treats server objects as resources that can be created or destroyed
+// Architecture style for designing networked applications
+// Can be used by virtually any programming language
+
+//HTTP METHODS
+// GET: Retrieve datat from a specified resource
+// POST: Submit data to be processed to a specified resource
+// PUT: Update a specified resource
+// DELTE: Delete a spedcified resource
+// HEAD, OPTIONS, PATCH
+
+//ENDPOINTS
+
+// The URI/URL where api/service can be accessed by a client application
+// GET - https://mysite.com/api/users
+// GET - https://mysite.com/api/users/1
+// POST - https://mysite.com/api/users
+// PUT - https://mysite.com/api/users/1
+// DELETE - https://mysite.com/api/users/1
