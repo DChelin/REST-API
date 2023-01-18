dbe378b0eb8d4535ab73a96da56d344e8174270b
DChelin <devlynchelin6@gmail.com>
2023-01-18 09:33:44 +0200

    18-Jan-2023 PHP API Notes
    Adding Classes to initialize.php
    Loading config.php
    Adding INC_PATH and CORE_PATH in initialize.php
    
    ----------------------------------
    
    Creating classes in post.php
    Post Properties
    Constructor with DB Connection
    Getting Posts from DB
    
    ------------------------------------
    
    Creation of read.php file for next phase

--- core/initialize.php
+++ core/initialize.php
@@ -1,6 +1,17 @@
 <?php
 
 defined('DS') ? null : define ('DS', DIRECTORY_SEPARATOR):
-defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'Documents' .DS. 'Github' .DS. 'REST-API');\
+defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'Documents' .DS. 'Github' .DS. 'REST-API');
+//Root - Documents/Github/REST-API/includes
+defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT .DS. 'includes');
+defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT .DS. 'core');
+
+// Load the config file first
+
+
+require_once(INC_PATH.DS. "config.php");
+
+//Core Classes
+require_once(CORE_PATH.DS. "post.php");
 
 ?>
