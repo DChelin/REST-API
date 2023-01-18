e8f58549316cec627c2d1e02bdcc686f5738a724
DChelin <devlynchelin6@gmail.com>
2023-01-18 08:16:26 +0200

    PDO Notes 18-Jan-2023
    PDO Notes 18-Jan-2023

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,5 @@
 <?php
+//PDO - PHP Data Objects
 
 $db_user = 'root';
 $db_password = '';
@@ -6,7 +7,7 @@ $db_name = 'restapi';
 
 $db = new PDO('mysql:host=127.0.0.1;dbname='.$db_name.';charset=utf8',$db_user,$db_password);
 
-// set some database attributes
+// Set some database attributes
 $db-> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $db-> setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
 $db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
